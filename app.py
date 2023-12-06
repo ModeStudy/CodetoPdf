99717df396014db044405c1d43781f10bb9f6309
Marco Olvera <marcolvera333@gmail.com>
2023-12-05 22:28:47 -0600

    se realizaron algunas pruebas y se llegaron a algunas conclusiones

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 
 from reportlab.pdfgen import canvas
+import sys
 
 # Read the content of the file
-file_path = "./pruebas.cpp"
+
+file_path = sys.argv[1]
 with open(file_path, "r") as file:
     content = file.read()
 
 # Create a PDF with the content
 pdf_path = "./pruebas.pdf"
 c = canvas.Canvas(pdf_path)
-text_object = c.beginText(100, 750)
+text_object = c.beginText(12, 769.89)# 72 = 1 inch como margen
+text_object.setFont("Times-Roman", 12)
+text_object.setLeading(13.8)
 text_object.textLines(content)
+
 c.drawText(text_object)
 c.save()
 
