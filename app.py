6eb66eaac8a092b8bf85b9e75ccd1a0450263819
Marco Olvera <marcolvera333@gmail.com>
2023-12-06 19:45:12 -0600

    primera versión funcional de la caracteristica de leer un archivo e imprimirlo correctamente en un archivo pdf y que se vea bien

--- app.py
+++ app.py
@@ -2,21 +2,43 @@
 from reportlab.pdfgen import canvas
 import sys
 
-# Read the content of the file
-
-file_path = sys.argv[1]
-with open(file_path, "r") as file:
-    content = file.read()
-
-# Create a PDF with the content
+limiteLineas = 51
+imprimir = ""
 pdf_path = "./pruebas.pdf"
+
+file_path = sys.argv[1] # es el path del archivo que se le pasa como argumento
+with open(file_path, "r") as file:
+    content = file.read() #lee el contenido del archivo
+
+
+
+lines_code = content.split('\n') # es una lista de lineas de codigo
+ultima_linea = len(lines_code) # obtengo la ultima linea del archivo su indice
+contador = 0
+
 c = canvas.Canvas(pdf_path)
-text_object = c.beginText(12, 769.89)# 72 = 1 inch como margen
-text_object.setFont("Times-Roman", 12)
-text_object.setLeading(13.8)
-text_object.textLines(content)
+def imprimirLineas():
+    global contador
+    global imprimir
+    global c
+    text_object = c.beginText(72, 769.89)# 72 = 1 inch como margen
+    text_object.setFont("Times-Roman", 12)
+    text_object.setLeading(13.8)
+    text_object.textLines(imprimir)
+    c.drawText(text_object)
+    c.showPage()
+    contador = 0
+    imprimir = ""
 
-c.drawText(text_object)
+for contadorLinea, line in enumerate(lines_code, start=1):
+    if contador < limiteLineas: # esto solamente es falso cuando cuando contador es igual a 51, si contador no llega a 51 no se imprime nada
+        imprimir = imprimir + line + "\n"
+        contador = contador + 1
+        if contadorLinea == ultima_linea:
+            imprimirLineas()
+    else:
+        imprimirLineas()
+
 c.save()
-
-print("PDF created successfully!")
+# Create a PDF with the content
+print("PDF created successfully!"
